9cd3c028f3993e2d62e4cf335fcb943309116ac1
J-mefdaoui <mefdaouijoseph@gmail.com>
2026-03-11 01:41:53 +0100

    messing around with login ui

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -1,49 +1,63 @@
 import 'package:flutter/material.dart';
 
-class LoginPage extends StatelessWidget{
-    const LoginPage({super.key});
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
 
-    @override
-    Widget build(BuildContext context){
-        return Scaffold(
-            backgroundColor: const Color.fromARGB(255, 223, 240, 224),
-            body: SafeArea(
-              child: Center(
-                child: Column(
-                    children: const [
-                        
-                        SizedBox(height: 100,),
-                        //logo
-                        Icon(
-                            Icons.lock,
-                            size: 100,
-                            color: Colors.green,
-                            ),
-                        SizedBox(height: 60,),
-                        
-                        //message
-                        Text(
-                            "welcome back",
-                            style: TextStyle(fontSize: 25, color: Colors.green, fontWeight: FontWeight.bold),
-                            ),
-                        //textfield
-                        InputDecorator(
-                            decoration: InputDecoration( border: OutlineInputBorder()),
-                            
-                        ),
-                        //textfield
-                        TextField(
-                            decoration: InputDecoration(border: OutlineInputBorder()),
-                        )
-                        //password
-                
-                        //sign-in|sign-up button
-                
-                        //etc
-                    ]
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 223, 240, 224),
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            children: const [
+              SizedBox(height: 100),
+              //logo
+              Icon(Icons.lock, size: 100, color: Colors.green),
+              SizedBox(height: 60),
+
+              //message
+              Text(
+                "welcome back",
+                style: TextStyle(
+                  fontSize: 25,
+                  color: Colors.green,
+                  fontWeight: FontWeight.bold,
                 ),
               ),
-            )
-        );
-    }
+              //textfield
+              Padding(
+                padding: EdgeInsetsGeometry.directional(
+                  start: 50,
+                  end: 50,
+                  top: 25,
+                  bottom: 10,
+                ),
+                child: TextField(
+                  decoration: InputDecoration(border: OutlineInputBorder()),
+                ),
+              ),
+              //textfield
+              Padding(
+                padding: EdgeInsetsGeometry.directional(
+                  start: 50,
+                  end: 50,
+                  top: 10,
+                  bottom: 50,
+                ),
+                child: TextField(
+                  decoration: InputDecoration(border: OutlineInputBorder()),
+                ),
+              ),
+              //password
+
+              //sign-in|sign-up button
+
+              //etc
+            ],
+          ),
+        ),
+      ),
+    );
+  }
 }
